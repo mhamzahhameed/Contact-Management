f6a93cb7775b26a0b872727c8d2e04fa29ed9b47
Mhamzahhameed <hamzahraja11@gmail.com>
2022-06-05 12:46:48 +0500

    API endpoints
    Created API endpoints

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const express = require('express')
-const connetDB = require('./config/db')
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 // Connect to MongoDB
-connetDB()
+connectDB();
 
-app.get('/', (req, res) => res.json( {msg: 'Welcome to the Contact Manager API'}))
+// Init Middleware
+app.use(express.json({ extended: false }))
 
 // Define Routes here
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-const PORT = process.env.PORT || 5000
+// Serve static files on production server
+if(process.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()  => console.log(`Server started on port ${PORT}`));
